756c729fa4f39233768455a7a1e9d06086b60ac6
rkoyanagui <39101149+rkoyanagui@users.noreply.github.com>
2021-12-31 07:11:22 -0300

    Bugfix/inspection (#2)
    * fix: static code analysis
    
    Correct various static code analysis problems.
    
    * fix: remove unused dependencies
    
    * chore: simplify fixed duration wait

--- src/main/java/com/rkoyanagui/wrappers/AssertionWrapper.java
+++ src/main/java/com/rkoyanagui/wrappers/AssertionWrapper.java
@@ -3,14 +3,15 @@ package com.rkoyanagui.wrappers;
 import java.util.concurrent.atomic.LongAdder;
 import org.awaitility.core.ConditionTimeoutException;
 import org.awaitility.core.ThrowingRunnable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class AssertionWrapper
 {
-  private static final Logger LOG = LoggerFactory.getLogger(AssertionWrapper.class);
   protected static final String BLEW_MAX_ATTEMPTS_MSG =
       "Reached max no. of attempts (%d). Will not retry.";
+
+  protected AssertionWrapper()
+  {
+  }
 
   public static ThrowingRunnable wrap(
       final ThrowingRunnable assertion,
@@ -22,10 +23,8 @@ public class AssertionWrapper
     {
       final int count = counter.intValue();
       counter.increment();
-      LOG.debug("Attempt no.={}, max_attempts={}", count, maxAttempts);
       if (count < maxAttempts)
       {
-        LOG.debug("Retrying...");
         if (count > 0)
         {
           // If this is not the very first try, so it is the second or third or so on try,
@@ -33,34 +32,15 @@ public class AssertionWrapper
           // supplying the last value, or because the value failed the test.
           // Which means it is the right time to perform some kind of action
           // that may turn things around, right before a new value is supplied.
-          LOG.debug("Performing 'orElseDo' action...");
-          try
-          {
-            orElseDo.run();
-          }
-          catch (Exception x)
-          {
-            LOG.debug("Error when performing 'orElseDo' action.", x);
-            throw x;
-          }
+          orElseDo.run();
         }
         // If the count is below the maximum number of attempts,
         // then tries to supply a new value for evaluation.
-        try
-        {
-          assertion.run();
-        }
-        catch (Exception x)
-        {
-          LOG.debug("Error when trying to supply a new value for evaluation.", x);
-          throw x;
-        }
+        assertion.run();
       }
       else
       {
-        final String msg = String.format(BLEW_MAX_ATTEMPTS_MSG, maxAttempts);
-        LOG.debug(msg);
-        throw new ConditionTimeoutException(msg);
+        throw new ConditionTimeoutException(String.format(BLEW_MAX_ATTEMPTS_MSG, maxAttempts));
       }
     };
   }
